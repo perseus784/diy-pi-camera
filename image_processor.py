a8e9fbabf71085ada73485bf65134b6894b52e21
perseus784 <ipaar3@gmail.com>
2024-11-29 14:16:18 -0600

    working image capture

--- image_processor.py
+++ image_processor.py
@@ -4,6 +4,7 @@ import os
 
 def process_image(camera, capture_config):
     image_file_name = os.path.join(image_path, "DSC_"+datetime.today().strftime('%Y-%m-%d-%H-%M-%S')+".jpg")
-    image = camera.switch_mode_and_capture_image(capture_config, image_file_name)
+    image = camera.switch_mode_and_capture_request("still")
+    image.save('main', image_file_name)
 
-    return None
+    return image_file_name
--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ import os
 
 image_path = os.path.join("images")
 shutter_button_pin =  16
-resolution_w, resolution_h = (4056, 3040)
-prw_res_w, prw_res_h = (640,320)
+resolution_w, resolution_h = (1920, 1080)
+prw_res_w, prw_res_h = (1280,720)
